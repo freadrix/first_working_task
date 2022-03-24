ccb7b9be72f3ed1dc1be739937bec5e00f77bba8
Anton Medvedev <anton.medvedev.1423@gmail.com>
2022-03-24 23:42:31 +0100

    feat: made mconstants in config

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
+import datetime
+
+
+today_date = datetime.datetime.now()
+month_before_today_date = today_date - datetime.timedelta(days=30)
+
+DEFAULT_SINCE_DATE = today_date.strftime("%Y-%m-%d")           # date format is YYYY-MM-DD
+DEFAULT_UNTIL_DATE = month_before_today_date.strftime("%Y-%m-%d")
+DEFAULT_KEYWORD = "unretirement"
 API_RELENG_ISSUES_ENDPOINT = "https://pagure.io/api/0/releng/issues"
-API_KEY = "******"                 # your API KEY
+API_KEY = "<your_api_key>"                 # your API KEY
