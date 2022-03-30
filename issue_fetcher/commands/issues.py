4ff3ea94c7e5670d1c7ff28e8b116005ef2385ca
Anton Medvedev <anton.medvedev.1423@gmail.com>
2022-03-30 15:08:38 +0200

    feat: change argument type to datetime for better error fetching

--- issue_fetcher/commands/issues.py
+++ issue_fetcher/commands/issues.py
@@ -13,11 +13,17 @@ class Context:
 
 @click.group()
 @click.option("-k", "--keyword", type=str, help="Issues with this keyword")
-@click.option("-ds", "--date-since", type=str, help="Issues since date in format 'YYYY-MM-DD'")
-@click.option("-du", "--date-until", type=str, help="Issues until date in format 'YYYY-MM-DD'")
+@click.option("-ds", "--date-since", type=click.DateTime(formats=["%Y-%m-%d"]), help="Issues since date in format "
+                                                                                     "'YYYY-MM-DD'")
+@click.option("-du", "--date-until", type=click.DateTime(formats=["%Y-%m-%d"]), help="Issues until date in format "
+                                                                                     "'YYYY-MM-DD'")
 @click.pass_context
 def cli(ctx, keyword, date_since, date_until):
     """Fetch issues"""
+    if date_since is not None:
+        date_since = date_since.strftime("%Y-%m-%d")
+    if date_until is not None:
+        date_until = date_until.strftime("%Y-%m-%d")
     ctx.obj = Context(keyword, date_since, date_until)
 
 
